a8007d1d509876e858d4e7d4dc6de9cc08548eac
valeri <valeri.tsertsvadze.4@gmail.com>
2018-08-06 09:39:12 +0400

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Collaboration Board built with React.js & Redux
+
+![Screenshot](https://raw.githubusercontent.com/valerit/collaboration/master/screenshot.png)
+
+## Phases
+
+The project is a test project.
+
+Thanks, David
+
---
+++ src/containers/App.jsx
@@ -0,0 +1,79 @@
+// @flow
+
+import React from 'react';
+
+import lanesActions from '../redux/actions/lanes';
+import { connect } from 'react-redux';
+import Lanes from '../components/Lanes/Lanes.jsx';
+
+import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContext } from 'react-dnd';
+
+import type { ILane } from '../types';
+
+type Props = {
+  lanes: ILane[],
+  onCreateLane: Function,
+  onDeleteLane: Function,
+  onEditLane: Function,
+  onMoveLane: Function,
+  onReset: Function,
+};
+
+class App extends React.Component<*, Props, *> {
+  render() {
+    return (
+      <div className="react-kanban">
+        <div className="app-title"><h1>Collaboration</h1> <small>David, Valeri </small></div>
+        
+        <button className="add-lane" onClick={this.props.onCreateLane}>
+          + List
+        </button>
+        <button className="reset-store" onClick={this.props.onReset}>
+          Reset
+        </button>
+        <Lanes
+          lanes={this.props.lanes}
+          onEditLane={this.props.onEditLane}
+          onDeleteLane={this.props.onDeleteLane}
+          onMoveLane={this.props.onMoveLane}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  lanes: state.lanes,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onCreateLane() {
+    dispatch(lanesActions.createLane('Active'));
+  },
+
+  onEditLane(laneId, name) {
+    const updatedLane: any = {
+      id: laneId,
+    };
+
+    if (name) {
+      (updatedLane: any).name = name;
+      (updatedLane: any).editing = false;
+    } else {
+      (updatedLane: any).editing = true;
+    }
+
+    dispatch(lanesActions.updateLane(updatedLane));
+  },
+
+  onDeleteLane(laneId) {
+    dispatch(lanesActions.deleteLane(laneId));
+  },
+
+  onMoveLane(sourceId, targetId) {
+    dispatch(lanesActions.move('lane', sourceId, targetId));
+  },
+});
+
+export default DragDropContext(HTML5Backend)(connect(mapStateToProps, mapDispatchToProps)(App));
